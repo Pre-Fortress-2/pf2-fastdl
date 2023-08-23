01f845c2b9e65b193abc0369231aab16363d0a21
dani <dangreene0@gmail.com>
2023-08-23 17:46:19 -0400

    Create update-maps.sh
    added linux script

---
+++ update-maps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git pull
+git checkout pf2
+
+cd pf2/maps
+for i in *.bz2; do
+    [ -f "$i" ] || break
+    bzip2 -d $i
+done
+
+mv *.bsp ../../../maps
+cp *.res *.txt ../../../maps
+cd ..
+
+rm -rf ../../materials
+cp materials/ ../../
+
+rm -rf ../../models
+cp models/ ../../
+
+cp mapcycle.txt motd.txt ../../cfg
