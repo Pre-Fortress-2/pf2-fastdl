fe67542c783a420b2a398617e48251552cbeeeb6
dani <sourdanidev@gmail.com>
2024-10-30 19:25:47 -0400

    feat: mapcycle script now has option for including default maps
    - remove or rename `default_mapcycle.txt` to remove stock maps from the list

--- mapcycle.py
+++ mapcycle.py
@@ -1,27 +1,25 @@
+# Script by Sour Dani for use with any fast-dl server.
+# github.com/Pre-Fortress-2/fastdl-base
 from os import listdir, path
 
 def main():
     extension = ".bsp.bz2"
-    dir = "pf2/maps"
-        
-    motd = """Map List:
-
-"""
-
-    f = open("mapcycle.txt", "w")
-    for file in listdir(dir):
-        if file.endswith(extension):
-            f.write(path.join(file[:-8]) + "\n")
-    f.close()
-
-    fm = open("motd.txt", "w")
-    fm.write(motd)
-    for file in listdir(dir):
-        if file.endswith(extension):
-            fm.write(path.join(file[:-8]) + "\n")
-    fm.close()
-        
+    dir = "pf2/maps" 
+    try:
+        with open("mapcycle.txt", "w") as mapcycle, open("motd.txt", "w") as motd:
+            motd.write("Map List:\n\n")
+            if path.isfile("default_mapcycle.txt"):
+                with open("default_mapcycle.txt") as default_maps:
+                    mapcycle.write(default_maps.read())
+                motd.write("- Includes stock maps\n")
+            for file in listdir(dir):
+                if file.endswith(extension):
+                    mapcycle.write(path.join(file[:-8]) + "\n")
+                    motd.write(path.join(file[:-8]) + "\n")
+    except Exception as ex:
+        print(ex)
 if __name__ == "__main__":
     main()
 
+    
     
