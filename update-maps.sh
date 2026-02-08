103b252de378c9a56a45afd5d5a893b43ec1d014
Sour Dani <sourdanidev@gmail.com>
2026-02-08 23:04:52 +0000

    feat: update script removes outdated files

--- update-maps.sh
+++ update-maps.sh
@@ -1,12 +1,36 @@
-#!/bin/bash
+    #!/bin/env bash
 git pull
 git clean -df
 git restore .
 
+echo Extracting map archives
 cd maps
 for i in *.bz2; do
-    [ -f "$i" ] || break
-    bzip2 -vd $i
+    if [ -f "$i" ]; then
+        bzip2 -vd $i
+    fi
+done
+
+echo Removing outdated map files
+suffixes=(
+    .res
+    _*.txt
+)
+
+for i in *.bsp; do
+
+    if [ -f "${i}.bz2" ]; then 
+        continue
+    fi
+
+    map=${i%".bsp"}
+    echo Removing $map
+
+    for s in "${suffixes[@]}"; do
+        rm ${map}${s}
+    done
+
+    rm $i
 done
 
 cd ..
